0657a9055909375dee2ee0c249929ec37bdef095
Kirstie <39728053+epixieme@users.noreply.github.com>
2023-07-24 12:03:16 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <Footer className="footer-container">
+    <footer className="footer-container">
       <p>2023 Kirstie Hayes</p>
-    </Footer>
+    </footer>
   );
 }
